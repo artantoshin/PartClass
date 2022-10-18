9a55defee99e5eccf0772f61a87be078108d7b6c
art.antoshin <art.antoshin1992@gmail.com>
2022-10-18 23:07:57 +0400

    Добавьте файлы проекта.

---
+++ PersonEat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartClass
+{
+    public partial class Person
+    {
+        public void Eat()
+        {
+            Console.WriteLine("I am eating");
+        }
+        partial void Read()
+        {
+            Console.WriteLine("I am reading a book");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using PartClass;
+
+Person tom = new Person();
+tom.Move();
+tom.Eat();
+tom.DoSomething();
+
+Console.ReadKey();
